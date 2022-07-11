82a1896688f4e9b547cbcfe25c6de513f650ce0b
Igor Candido Rodrigues <igorcr39@gmail.com>
2022-07-11 09:17:46 -0300

    modifing /register route

--- shop/admin/routes.py
+++ shop/admin/routes.py
@@ -1,11 +1,12 @@
 from flask import render_template, session, request, redirect, url_for, flash
 
-from shop import app, db
+from shop import app, db, bcrypt
 from shop.admin.forms import RegistrationForm
+from shop.admin.models import User
 
 @app.route('/')
 def home():
-    return 'Home page of your shop'
+    return render_template('admin/index.html', title='Admin Page')
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -13,6 +14,10 @@ def register():
     form=RegistrationForm(request.form)
     #this is a result of sended form, with all information
     if request.method == 'POST' and form.validate():
-        flash('thanks for registring')
-        return redirect(url_for('login'))
+        hash_password = bcrypt.generate_password_hash(form.password.data)
+        user = User(name=form.name.data, username=form.username.data, email=form.email.data, password=hash_password)
+        db.session.add(user)
+        db.session.commit()
+        flash(f'Welcome, {form.name.data}. Thanks for registring', 'success')
+        return redirect(url_for('home'))
     return render_template('admin/register.html', form=form, title='Registration page')
