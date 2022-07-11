54c865dbd4fc20da5fa8c885d10f16ba196b2e40
Igor Candido Rodrigues <igorcr39@gmail.com>
2022-07-11 07:31:52 -0300

    add route from /register

--- shop/admin/routes.py
+++ shop/admin/routes.py
@@ -1,12 +1,18 @@
-from flask import render_template, session, request, redirect, url_for
+from flask import render_template, session, request, redirect, url_for, flash
 
 from shop import app, db
+from shop.admin.forms import RegistrationForm
 
 @app.route('/')
 def home():
     return 'Home page of your shop'
 
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template('admin/register.html', title='Register user')
+    form=RegistrationForm(request.form)
+    #this is a result of sended form, with all information
+    if request.method == 'POST' and form.validate():
+        flash('thanks for registring')
+        return redirect(url_for('login'))
+    return render_template('admin/register.html', form=form, title='Registration page')
