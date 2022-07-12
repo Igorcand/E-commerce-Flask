35f780086a41b433bf6e21433e6579d85d968183
Igor Candido Rodrigues <igorcr39@gmail.com>
2022-07-12 06:51:45 -0300

    ceate a model form to products

---
+++ shop/products/forms.py
@@ -0,0 +1,18 @@
+from wtforms import BooleanField, StringField, PasswordField, validators , ValidationError, IntegerField, TextAreaField
+from flask_wtf import Form
+from flask_wtf.file import FileAllowed, FileField, FileRequired
+
+#WTFForm is a flexible form validarion and rendering library for Python, we can use to simplify the usabity of HTML form. Including a verifying on password and confirming password, validation if on field email is a real email
+
+class Addproducts(Form):
+    name = StringField('Name', [validators.DataRequired()])
+    price = IntegerField('price', [validators.DataRequired()])
+    discount = IntegerField('Discount', default=0)
+    stock = IntegerField('Stock', [validators.DataRequired()])
+    description = TextAreaField('Description', [validators.DataRequired()])
+    color = TextAreaField('Color', [validators.DataRequired()])
+
+    image_1 = FileField('Image 1', validators=[FileRequired(), FileAllowed(['jpg', 'png', 'gif', 'jpeg'])])
+    image_2 = FileField('Image 2', validators=[FileRequired(), FileAllowed(['jpg', 'png', 'gif', 'jpeg'])])
+    image_3 = FileField('Image 3', validators=[FileRequired(), FileAllowed(['jpg', 'png', 'gif', 'jpeg'])])
+
