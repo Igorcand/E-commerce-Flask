5d44435af9a8997c52d788c538091d24a3231484
Igor Candido Rodrigues <igorcr39@gmail.com>
2022-07-13 06:49:53 -0300

    inicialize module on mainly file

--- shop/__init__.py
+++ shop/__init__.py
@@ -1,10 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import  Bcrypt
+from flask_uploads import UploadSet, configure_uploads, IMAGES
+import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///myshop.db'
 app.config['SECRET_KEY'] = 'banana'
+
+app.config['UPLOADED_PHOTOS_DEST'] = os.path.join(basedir, 'static/images')
+photos = UploadSet('photos', IMAGES)
+configure_uploads(app, photos)
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
