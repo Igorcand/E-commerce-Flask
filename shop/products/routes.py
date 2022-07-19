57db68424c0359a592e2c93ac82c1fd2ece35a74
Igor Candido Rodrigues <igorcr39@gmail.com>
2022-07-19 07:25:49 -0300

    got better the code removing duplicated lines

--- shop/products/routes.py
+++ shop/products/routes.py
@@ -5,21 +5,25 @@ from shop.products.models import Brand, Category, Addproduct
 from shop.products.forms import Addproducts
 import secrets, os
 
+def brands():
+    brands = Brand.query.join(Addproduct, (Brand.id ==Addproduct.brand_id)).all()
+    return brands
+
+def categories():
+    categories = Category.query.join(Addproduct, (Category.id ==Addproduct.category_id)).all()
+    return categories
+
 @app.route('/')
 def home():
     page = request.args.get('page', 1, type=int)
     products = Addproduct.query.filter(Addproduct.stock>0).order_by(Addproduct.id.desc()).paginate(page=page, per_page=8)
-    brands = Brand.query.join(Addproduct, (Brand.id ==Addproduct.brand_id)).all()
-    categories = Category.query.join(Addproduct, (Category.id ==Addproduct.category_id)).all()
-    return render_template('/products/index.html', products=products, brands=brands, categories=categories)
+    return render_template('/products/index.html', products=products, brands=brands(), categories=categories())
 
 
 @app.route('/product/<int:id>')
 def single_page(id):
     product = Addproduct.query.get_or_404(id)
-    brands = Brand.query.join(Addproduct, (Brand.id ==Addproduct.brand_id)).all()
-    categories = Category.query.join(Addproduct, (Category.id ==Addproduct.category_id)).all()
-    return render_template('products/single_page.html', product=product, brands=brands, categories=categories)
+    return render_template('products/single_page.html', product=product, brands=brands(), categories=categories())
 
 
 @app.route('/brand/<int:id>')
@@ -28,18 +32,14 @@ def get_brand(id):
     page = request.args.get('page', 1, type=int)
 
     brand= Addproduct.query.filter_by(brand=get_b).paginate(page=page, per_page=8)
-    brands = Brand.query.join(Addproduct, (Brand.id ==Addproduct.brand_id)).all()
-    categories = Category.query.join(Addproduct, (Category.id ==Addproduct.category_id)).all()
-    return render_template('/products/index.html', brand=brand, brands=brands, categories=categories, get_b=get_b)
+    return render_template('/products/index.html', brand=brand, brands=brands(), categories=categories(), get_b=get_b)
 
 @app.route('/categories/<int:id>')
 def get_category(id):
     page = request.args.get('page', 1, type=int)
     get_cat = Category.query.filter_by(id=id).first_or_404()
     get_cat_prod= Addproduct.query.filter_by(category=get_cat).paginate(page=page, per_page=8)
-    brands = Brand.query.join(Addproduct, (Brand.id ==Addproduct.brand_id)).all()
-    categories = Category.query.join(Addproduct, (Category.id ==Addproduct.category_id)).all()
-    return render_template('/products/index.html', get_cat_prod=get_cat_prod,  brands=brands, categories=categories, get_cat=get_cat)
+    return render_template('/products/index.html', get_cat_prod=get_cat_prod,  brands=brands(), categories=categories(), get_cat=get_cat)
 
 @app.route('/addbrand', methods=['GET', 'POST'])
 def addbrand():
