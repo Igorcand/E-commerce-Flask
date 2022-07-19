95326ead2fbafe7832358a8053c4f2d3df658a2a
Igor Candido Rodrigues <igorcr39@gmail.com>
2022-07-19 07:06:12 -0300

    add a new html and function to edit products on cart

--- shop/carts/carts.py
+++ shop/carts/carts.py
@@ -19,7 +19,7 @@ def addcart():
         colors = request.form.get('colors')
         product = Addproduct.query.filter_by(id=product_id).first()
         if product_id and quantity and colors and request.method=='POST':
-            DictItems = {product_id: {'name': product.name, 'price':product.price, 'discount': product.discount, 'color': colors, 'quantity': quantity, 'image': product.image_1}}
+            DictItems = {product_id: {'name': product.name, 'price':product.price, 'discount': product.discount, 'color': colors, 'quantity': quantity, 'image': product.image_1, 'colors': product.colors}}
 
             if 'Shoppingcart' in session:
                 print(session['Shoppingcart'])
@@ -49,3 +49,33 @@ def getCart():
         tax = ("%.2f" % (.06 * float(subtotal)))
         grandtotal = float("%.2f" % (1.06 *subtotal))
     return render_template('products/carts.html', tax=tax, grandtotal=grandtotal)
+
+@app.route('/updatecart/<int:code>', methods=['GET', 'POST'])
+def updatecart(code):
+    if 'Shoppingcart' not in session and len(session['Shoppingcart'])<= 0:
+        return redirect(url_for('home'))
+    if request.method=='POST':
+        quantity = request.form.get('quantity')
+        color = request.form.get('color')
+        try:
+            session.modified = True 
+            for key, item in session['Shoppingcart'].items():
+                if int(key) == code:
+                    item['quantity'] = quantity
+                    item['color'] = color 
+                    flash(f'Items is updated!')
+                    return redirect(url_for('getCart'))
+
+        except Exception as e:
+            print(e)
+            return redirect(url_for('getCart'))
+
+
+
+@app.route('/empty')
+def empty_cart():
+    try:
+        session.clear()
+        return redirect(url_for('home'))
+    except Exception as e:
+        print(e)
