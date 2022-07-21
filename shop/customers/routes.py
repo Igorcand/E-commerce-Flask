4428d790149515f1b13cef75c17b795531a8bafa
Igor Candido Rodrigues <igorcr39@gmail.com>
2022-07-21 07:19:06 -0300

    put at customer/routes two callers. One to do a register of user and another to do login, using flask_login to do the validation

--- shop/customers/routes.py
+++ shop/customers/routes.py
@@ -1,9 +1,34 @@
 from flask import current_app, render_template, session, request, redirect, url_for, flash
-from shop import db, app, photos
-from shop.customers.forms import CustomerRegisterForm
+from flask_login import login_required, current_user, logout_user, login_user
+from shop import db, app, photos, bcrypt, login_manager
+from shop.customers.forms import CustomerRegisterForm, CustomerLoginForm
+from shop.customers.models import Register
+
 import secrets, os
 
 @app.route('/customer/register', methods=['GET', 'POST'])
 def customer_register():
-    form = CustomerRegisterForm(request.form)
+    form = CustomerRegisterForm()
+    if form.validate_on_submit():
+        hash_password = bcrypt.generate_password_hash(form.password.data)
+        register = Register(name=form.name.data, username=form.username.data, email=form.email.data, password=hash_password, country=form.country.data, state=form.state.data, city=form.city.data, contact=form.contact.data, address=form.address.data, zipcode=form.zipcode.data)
+        db.session.add(register)
+        flash(f'Welcome {form.name.data}. Thank you for registering', 'success')
+        db.session.commit()
+        return redirect(url_for('customerLogin'))
     return render_template('customer/register.html', form=form)
+
+@app.route('/customer/login', methods=['GET', 'POST'])
+def customerLogin():
+    form = CustomerLoginForm()
+    if form.validate_on_submit():
+        user = Register.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user)
+            flash('You are login now!', 'success')
+            next = request.args.get('next')
+            return redirect(url_for(next or'home'))
+        else:
+            flash('Incorrect email and password', 'danger')
+            return redirect(url_for('customerLogin'))
+    return render_template('/customer/login.html', form=form)
