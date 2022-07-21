1257092a9583da64c6e05841f96e93e1f8733ad8
Igor Candido Rodrigues <igorcr39@gmail.com>
2022-07-21 07:21:19 -0300

    create a new form to do login after registring and added validation if exist some username or email that are equal in database

--- shop/customers/forms.py
+++ shop/customers/forms.py
@@ -1,9 +1,11 @@
 from wtforms import BooleanField, StringField, PasswordField, validators , ValidationError, IntegerField, TextAreaField, DecimalField, SubmitField
 from flask_wtf import Form
 from flask_wtf.file import FileAllowed, FileField, FileRequired
+from flask_wtf import FlaskForm
+from .models import Register
 
-    
-class CustomerRegisterForm(Form):
+
+class CustomerRegisterForm(FlaskForm):
     name = StringField('Name', [validators.Length(min=4, max=25), validators.DataRequired()])
     username = StringField('Username', [validators.Length(min=4, max=25), validators.DataRequired()])
     email = StringField('Email Address', [validators.Length(min=6, max=35), validators.Email(), validators.DataRequired()])
@@ -17,10 +19,21 @@ class CustomerRegisterForm(Form):
     state = StringField('State', [validators.DataRequired()])
     city = StringField('City', [validators.DataRequired()])
     contact = StringField('Contat', [validators.DataRequired()])
-    adress = StringField('Adress', [validators.DataRequired()])
+    address = StringField('address', [validators.DataRequired()])
     zipcode = StringField('Zip code', [validators.DataRequired()])
 
     profile = FileField('Profile', validators=[FileAllowed(['jpg', 'png', 'jpeg', 'gif'])])
 
     submit = SubmitField('Register')
 
+    def validate_username(self, username):
+        if Register.query.filter_by(username=username.data).first():
+            raise ValidationError('This username is already in use!')
+        
+    def validate_email(self, email):
+        if Register.query.filter_by(email=email.data).first():
+            raise ValidationError('This email address is already in use!')
+
+class CustomerLoginForm(FlaskForm):
+    email = StringField('Email Address', [validators.Email(), validators.DataRequired()])
+    password = PasswordField('Password', [validators.DataRequired()])
